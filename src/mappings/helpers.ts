0da54fc5bce6c714b273fab5cd34cad0b44fd58f
1200 <120084601@qq.com>
2021-01-26 23:29:59 +0800

    bug fix

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xb0b670fc1F7724119963018DB0BfA86aDb22d941'
+export const FACTORY_ADDRESS = '0x1ea26c17d061e8a7cc33b20d8d8dad131d7fb392'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
